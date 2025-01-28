b21bb76010a56f114adfd8cd53e1f0777754acbe
Batebates <baptiste.decrand@laposte.net>
2025-01-28 23:56:31 +0100

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([43.606346535595776, 1.429172974796818], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,

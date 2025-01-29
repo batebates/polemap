11cacdc72eb1ffde0f2841b3056224f6aabdacd5
Batebates <baptiste.decrand@laposte.net>
2025-01-29 09:37:08 +0100

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,12 +1,4 @@
 
-var map = L.map('map').setView([43.606346535595776, 1.429172974796818], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-
 // 🔐 Stocker le hash du mot de passe (mot de passe : "monSuperMotDePasse123")
 const HASH_STOCKE = "6cc4f9e81df815c52e73b1562df40607"; // MD5("Amusez-vous")
 let map;

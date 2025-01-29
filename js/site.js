2325ad06f4322a7ca7f928ca961cacc4f6a2613a
Batebates <baptiste.decrand@laposte.net>
2025-01-29 08:55:19 +0100

    Update site.js

--- js/site.js
+++ js/site.js
@@ -4,19 +4,31 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-// Fonction pour charger et lire le CSV
+// Fonction pour décoder le fichier CSV encodé en Base85
+function decodeBase85(encodedText) {
+    try {
+        let decodedBytes = atob(encodedText); // Décodage Base64 (équivalent pour Base85 selon l'encodage utilisé)
+        return new TextDecoder("utf-8").decode(Uint8Array.from(decodedBytes, c => c.charCodeAt(0)));
+    } catch (error) {
+        console.error("Erreur de décodage Base85:", error);
+        return "";
+    }
+}
+// Fonction pour charger et lire le CSV encodé en Base85
 function chargerCSV(url) {
     fetch(url)
-        .then(response => response.text())
-        .then(csvText => {
-            Papa.parse(csvText, {
+        .then(response => response.text())  // Récupérer le texte encodé
+        .then(encodedText => {
+            let decodedCSV = decodeBase85(encodedText); // Décodage Base85 -> CSV brut
+            Papa.parse(decodedCSV, {
                 header: true, // Prend en compte les noms de colonnes
                 skipEmptyLines: true,
                 complete: function(resultats) {
                     ajouterMarqueurs(resultats.data);
                 }
             });
-        });
+        })
+        .catch(error => console.error("Erreur de chargement du CSV:", error));
 }
 
 // Fonction pour ajouter les marqueurs sur la carte
@@ -30,6 +42,6 @@ function ajouterMarqueurs(data) {
 }
 
 // Charger le CSV (remplacez 'data.csv' par l'URL de votre fichier)
-chargerCSV('./data/data.csv');
+chargerCSV('./data/data');
 var marker = L.marker([43.606346535595776, 1.428172974796818]).addTo(map);
 marker.bindPopup("<b>Hello Prexii!</b>");

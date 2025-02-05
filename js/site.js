53752e6341aff5b2765223d1ceaa58e825de2a08
Batebates <baptiste.decrand@laposte.net>
2025-02-05 21:08:25 +0100

    Update site.js

--- js/site.js
+++ js/site.js
@@ -2,10 +2,97 @@
 const HASH_STOCKE = "6cc4f9e81df815c52e73b1562df40607"; // MD5("Amusez-vous")
 const API_URL = "http://148.113.45.125:3000/lieux";
 let map;
+//define row context menu contents
+var rowMenu = [
+    {
+        label:"<i class='fas fa-user'></i> Change Name",
+        action:function(e, row){
+            row.update({name:"Steve Bobberson"});
+        }
+    },
+    {
+        label:"<i class='fas fa-check-square'></i> Select Row",
+        action:function(e, row){
+            row.select();
+        }
+    },
+    {
+        separator:true,
+    },
+    {
+        label:"Admin Functions",
+        menu:[
+            {
+                label:"<i class='fas fa-trash'></i> Delete Row",
+                action:function(e, row){
+                    row.delete();
+                }
+            },
+            {
+                label:"<i class='fas fa-ban'></i> Disabled Option",
+                disabled:true,
+            },
+        ]
+    }
+]
+
+//define column header menu as column visibility toggle
+var headerMenu = function(){
+    var menu = [];
+    var columns = this.getColumns();
+
+    for(let column of columns){
+
+        //create checkbox element using font awesome icons
+        let icon = document.createElement("i");
+        icon.classList.add("fas");
+        icon.classList.add(column.isVisible() ? "fa-check-square" : "fa-square");
+
+        //build label
+        let label = document.createElement("span");
+        let title = document.createElement("span");
+
+        title.textContent = " " + column.getDefinition().title;
+
+        label.appendChild(icon);
+        label.appendChild(title);
+
+        //create menu item
+        menu.push({
+            label:label,
+            action:function(e){
+                //prevent menu closing
+                e.stopPropagation();
+
+                //toggle current column visibility
+                column.toggle();
+
+                //change menu item icon
+                if(column.isVisible()){
+                    icon.classList.remove("fa-square");
+                    icon.classList.add("fa-check-square");
+                }else{
+                    icon.classList.remove("fa-check-square");
+                    icon.classList.add("fa-square");
+                }
+            }
+        });
+    }
+
+   return menu;
+};
+
 var table = new Tabulator("#bdd-table", {
     height:"311px",
     layout:"fitColumns",
     autoColumns:true
+    columns:[
+        {title:"Id", field:"id",sorter:"number", headerMenu:headerMenu},
+        {title:"Nom", field:"nom", hozAlign:"center", headerMenu:headerMenu},
+        {title:"Latitude", field:"latitude", headerMenu:headerMenu},
+        {title:"Longitude", field:"longitude", hozAlign:"center", headerMenu:headerMenu},
+        {title:"Adresse", field:"adress"}
+    ],
 });
 
 // Vérifier le mot de passe
